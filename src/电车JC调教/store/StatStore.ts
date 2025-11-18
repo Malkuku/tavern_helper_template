e3bef253d5afe98930a792e9a0e4d087837ac740
ka-cat <3159409764@qq.com>
2025-11-18 10:32:27 +0800

    fix: 修复楼层乱加载的bug

--- src/电车JC调教/store/StatStore.ts
+++ src/电车JC调教/store/StatStore.ts
@@ -7,32 +7,19 @@ import { StatData } from '../types/StatData';
 export const useStatStore = defineStore('stat', () => {
   // 状态数据
   const stat_data = ref<StatData>();
-  const initData = ()=>{
-    if(getCurrentMessageId() < getLastMessageId()-10){
+  const initData = async () => {
+    if (getCurrentMessageId() < getLastMessageId() - 10) {
       console.log(`正在加载位于第${getCurrentMessageId()}楼层的旧数据`);
       console.log('楼层过深,停止加载');
       return;
     }
-    if(getCurrentMessageId() < getLastMessageId()-1){
-      getOldStatData();
-    }else{
-      stat_data.value = getVariables({ type: 'chat' }).stat_data as StatData;
-    }
-    console.log(`loading stat_data at #${getCurrentMessageId()}`,stat_data.value)
+    await getOldStatData();
   }
 
   // 处理统计数据的函数
-  const processStatData = (detail: { stat: StatData }) => {
-    if(getCurrentMessageId() >= getLastMessageId()-1){
-      stat_data.value = detail.stat;
-      toastr.success('已获取最新数据');
-      console.log(`Stat data updated at ${getCurrentMessageId()}: `, stat_data.value);
-    }
-  };
-
-  // 处理统计数据的函数
-  const queryResult = (detail:{ result:{stat: StatData }}) => {
-    if(getCurrentMessageId() < getLastMessageId()-1){
+  const processStatData = (detail:{ result:{message_id:number,stat: StatData }}) => {
+    console.log(`Stat data updated at ${detail.result.message_id} and current messageId is ${getCurrentMessageId()}`);
+    if(getCurrentMessageId() == detail.result.message_id){
       stat_data.value = detail.result.stat;
       console.log(`Stat data loaded at ${getCurrentMessageId()}:`, stat_data.value);
     }
@@ -40,8 +27,7 @@ export const useStatStore = defineStore('stat', () => {
 
   // 注册事件监听器
   const registerListener = () => {
-    eventOn('era:writeDone', processStatData);
-    eventOn('era:queryResult',queryResult)
+    eventOn('era:queryResult', processStatData);
   };
 
   return {
