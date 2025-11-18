9900a0b265e7fb461eb8da0744159df160087395
ka-cat <3159409764@qq.com>
2025-11-18 09:40:07 +0800

    fix: 加载失败问题

--- src/电车JC调教/util/messageUtil.ts
+++ src/电车JC调教/util/messageUtil.ts
@@ -9,49 +9,14 @@ export const getCurrentMessage = () => {
 }
 
 
-export const getOldStatData = ()=>{
-  const variables = getVariables({ type: 'chat' })
-  const editLogs = variables.ERAMetaData.EditLogs;
+export const getOldStatData = async () => {
   const message = getCurrentMessage();
   const match = message.match(/"era-message-key"="([^"]+)"/);
   if (match) {
     const key = match[1];
-    if(key && editLogs[key]){
-      console.log(`正在加载位于第${getCurrentMessageId()}楼层的旧数据`);
-      const res = editLogsToJson(JSON.parse(editLogs[key]) as EditLog[]);
-      console.log(`旧数据`,res);
-      return res;
-    }
+    await eventEmit('era:getSnapshotAtMk', { mk: key });
+    return;
   }
   console.error(`未找到位于第${getCurrentMessageId()}楼层的旧数据`);
   return null;
-}
-type EditLog = {
-  op: 'update';
-  path: string;
-  value_old: any;
-  value_new: any;
-};
-
-function editLogsToJson(logs: EditLog[]): Record<string, any> {
-  if (!Array.isArray(logs)) return {};
-
-  const result: Record<string, any> = {};
-
-  for (const log of logs) {
-    const keys = log.path.split('.');
-    let target = result;
-
-    for (let i = 0; i < keys.length - 1; i++) {
-      const key = keys[i];
-      if (!target[key] || typeof target[key] !== 'object') {
-        target[key] = {};
-      }
-      target = target[key];
-    }
-
-    target[keys[keys.length - 1]] = log.value_new;
-  }
-
-  return result;
 }
