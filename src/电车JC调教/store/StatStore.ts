63672b3a81a81833cc8c0c1cf3a0335e2200902f
ka-cat <3159409764@qq.com>
2025-11-18 09:40:07 +0800

    fix: 加载失败问题

--- src/电车JC调教/store/StatStore.ts
+++ src/电车JC调教/store/StatStore.ts
@@ -7,7 +7,6 @@ import { StatData } from '../types/StatData';
 export const useStatStore = defineStore('stat', () => {
   // 状态数据
   const stat_data = ref<StatData>();
-
   const initData = ()=>{
     if(getCurrentMessageId() < getLastMessageId()-10){
       console.log(`正在加载位于第${getCurrentMessageId()}楼层的旧数据`);
@@ -15,7 +14,7 @@ export const useStatStore = defineStore('stat', () => {
       return;
     }
     if(getCurrentMessageId() < getLastMessageId()-1){
-      stat_data.value = stat_data.value = getOldStatData() as StatData;
+      getOldStatData();
     }else{
       stat_data.value = getVariables({ type: 'chat' }).stat_data as StatData;
     }
@@ -24,18 +23,25 @@ export const useStatStore = defineStore('stat', () => {
 
   // 处理统计数据的函数
   const processStatData = (detail: { stat: StatData }) => {
-    if(getCurrentMessageId() < getLastMessageId()-1){
-      return;
-    }else{
+    if(getCurrentMessageId() >= getLastMessageId()-1){
       stat_data.value = detail.stat;
       toastr.success('已获取最新数据');
+      console.log(`Stat data updated at ${getCurrentMessageId()}: `, stat_data.value);
     }
-    console.log('Stat data updated:', stat_data.value);
+  };
+
+  // 处理统计数据的函数
+  const queryResult = (detail:{ result:{stat: StatData }}) => {
+    if(getCurrentMessageId() < getLastMessageId()-1){
+      stat_data.value = detail.result.stat;
+      console.log(`Stat data loaded at ${getCurrentMessageId()}:`, stat_data.value);
+    }
   };
 
   // 注册事件监听器
   const registerListener = () => {
     eventOn('era:writeDone', processStatData);
+    eventOn('era:queryResult',queryResult)
   };
 
   return {
