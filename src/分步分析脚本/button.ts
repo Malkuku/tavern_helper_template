fe50422df79da6d2032f4d7f3d1baf1fa0a14066
ka-cat <3159409764@qq.com>
2025-12-07 10:37:51 +0800

    feat: 重发变量更新，错误回滚机制

--- src/分步分析脚本/button.ts
+++ src/分步分析脚本/button.ts
@@ -2,12 +2,19 @@
  * 注册酒馆助手按钮
  */
 import { useUiStore } from './UI/store';
+import { reSendEraUpdate } from './UI/handleEvents';
 
 $(() => {
-  replaceScriptButtons([{ name: '按钮', visible: true }]);
+  replaceScriptButtons([
+    { name: '分步分析设置', visible: true },
+    { name: '重新分析变量', visible: true }
+  ]);
 
-  eventOn(getButtonEvent('按钮'), () => {
+  eventOn(getButtonEvent('分步分析设置'), () => {
     useUiStore().showUI = !useUiStore().showUI;
-    console.log('按钮被点击');
+  });
+
+  eventOn(getButtonEvent('重新分析变量'), async () => {
+    await reSendEraUpdate();
   });
 });
